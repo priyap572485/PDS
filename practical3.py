3914eee561d4e30d2b2fd7b1e2037584d3328459
priya prajapati <priyap572485@gmail.com>
2026-08-10 22:30:44 +0530

    practical3.py

---
+++ practical3.py
@@ -0,0 +1,39 @@
+a  = 1
+b = 1.0
+c = -999
+
+a = '1'
+
+type(a)
+
+l = []
+l = list()
+
+l = [1,2,3,4,5]
+l[0]
+l[-1]
+l[2:4]
+l = [1,'string',[1,2,3]]
+print[l]
+for i in l:
+print(i)
+for i in range(len(i)):
+print(l(i))
+
+t = ('apple','bottle','car')
+type(t)
+t[o]
+t[0] = ('apple')
+len(t)
+
+d = {'a':1,'b':2}
+d['a']
+d['b']
+d['c=3']
+d
+d.iteam()
+
+d.key()
+
+d.values()
+
