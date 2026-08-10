2c670400081bf81a3a920fc89ef39cc87c291992
priya prajapati <priyap572485@gmail.com>
2026-08-10 22:30:14 +0530

    prcatical 2.py

---
+++ prcatical 2.py
@@ -0,0 +1,15 @@
+def function_name():
+    + + +
+    + + +
+
+    def helloworld():
+    print{'helloworld' this is my first function}
+
+def add(x,y):
+    return x+y
+add(5,4)
+
+type(helloworld())
+
+type(None)
+
